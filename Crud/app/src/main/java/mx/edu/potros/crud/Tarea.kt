06bb7893ce764d93387c993658e8a82a19bda682
michelle <michellebojorquezsterling@gmail.com>
2025-07-02 23:56:17 -0700

    CRUD

---
+++ Crud/app/src/main/java/mx/edu/potros/crud/Tarea.kt
@@ -0,0 +1,9 @@
+package mx.edu.potros.crud
+
+data class Tarea(var id: String="", var titulo: String="", var descripcion: String=""){
+    fun toMap(): Map<String, String>{
+        return mapOf(
+            "titulo" to titulo, "descripcion" to descripcion
+        )
+    }
+}
